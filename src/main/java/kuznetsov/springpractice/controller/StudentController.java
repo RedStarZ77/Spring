785adf4d7abb4ad478546cb3e9c5fe53779698f7
RedStarZ77 <redstarz2002@mail.ru>
2025-04-15 12:26:58 +0300

    Spring practice

---
+++ src/main/java/kuznetsov/springpractice/controller/StudentController.java
@@ -0,0 +1,10 @@
+package kuznetsov.springpractice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+}
